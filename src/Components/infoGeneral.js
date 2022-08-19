ad8f9a2503527ab3d80086373d06fdf23640a97e
Mandarinoazul <100236557+Mandarinoazul@users.noreply.github.com>
2022-08-19 01:50:59 -0400

    añadir boton git

--- src/Components/infoGeneral.js
+++ src/Components/infoGeneral.js
@@ -9,6 +9,8 @@ function infoGeneral() {
             <p> Mi nombre es, Daniel EC Ramírez.</p>
             <p> Mi vocación es Desarrollador de Software.</p>
             <p>Contactame a través de las siguientes maneras: </p>
+            <br />
+            <button>Github</button>
 
 
         </section>
